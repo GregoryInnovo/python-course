f7a51f633de43625f97642f602b6047146eb7fe3
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-01-06 23:32:21 -0500

    improve project with functions and conditionals

---
+++ basic/condicionales.py
@@ -0,0 +1,15 @@
+""" age = int(input("Write your age: "))
+
+# You can write pass to code later
+if age >= 18:
+    print("You are of age")
+else:
+    print("You are under age") """
+
+num = int(input("Write a number "))
+if num > 5:
+    print("Is greater than 5")
+elif num == 5:
+    print("Equals 5")
+else:
+    print("Is less than 5")
--- basic/conversor.py
+++ basic/conversor.py
@@ -1,14 +1,36 @@
-user_cop = float(input("How much COP you have? "))
-usd_value = 4032.67
 
-result_usd = user_cop / usd_value
-result_usd = round(result_usd, 2)
-result_usd = str(result_usd)
-print("You have $" +result_usd + " usd!")
+def convert(current_type, usd_value):
+    user_pesos = float(input("How much " + current_type + " you have? "))
+    result_usd = user_pesos / usd_value
+    result_usd = round(result_usd, 2)
+    result_usd = str(result_usd)
+    print("You have $" +result_usd + " usd!")
 
-user_usd = float(input("How much USD you have? "))
+menu = """
+Welcome to convert currency 💲
 
-result_cop = user_usd * usd_value
-result_cop = round(result_cop, 2)
-result_cop = str(result_cop)
-print("You have $" +result_cop + " cop!")
+1 - COP
+2 - ARG
+3 - MEX
+
+Choose an option: """
+
+option = int(input(menu))
+
+if option == 1:
+    convert("COP", 4032.67)
+elif option == 2:
+    convert("ARG", 65)
+elif option == 3:
+    convert("MEX", 24)
+else:
+    print("Please select an exist option ")
+
+
+
+# user_usd = float(input("How much USD you have? "))
+
+# result_cop = user_usd * usd_value
+# result_cop = round(result_cop, 2)
+# result_cop = str(result_cop)
+# print("You have $" +result_cop + " cop!")
